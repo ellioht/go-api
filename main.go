df00d4d181e41b50872bf453ec9ae483884fa536
Elliot <130664947+ellioht@users.noreply.github.com>
2023-08-18 15:52:11 +0100

    middleware function

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
+func ContextMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mydata := chi.URLParam(r, "mydata")                          // Get the value from the URL
+		ctx := context.WithValue(r.Context(), "mydataValue", mydata) // Add it to the context
+		next.ServeHTTP(w, r.WithContext(ctx))                        // Call the next handler
+	})
+}
+
 func main() {
 	// Create a new Router
-	router := chi.NewRouter()
+	r := chi.NewRouter()
 
 	// GET
-	router.Get("/api/v1/test/{mydata}", func(w http.ResponseWriter, r *http.Request) {
-		message := chi.URLParam(r, "mydata")
-		w.Write([]byte(message))
+	r.With(ContextMiddleWare).Get("/api/v1/test/{mydata}", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()                               // Get the context
+		mydataValue := ctx.Value("mydataValue").(string) // Get the value from the context
+		w.Write([]byte(mydataValue))
 	})
 
 	// Start the server
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8000", r)
 }
